f882f03c41a902b1fa8680329177f284ff0a0c47
Sus.diaz <sus.diazse@gmail.com>
2025-10-21 17:34:43 +0200

    Add event listeners

--- js/main.js
+++ js/main.js
@@ -4,16 +4,8 @@ const kittenOne = document.querySelector(".js-list");
 const kittenTwo = document.querySelector(".js-list");
 const kittenThree = document.querySelector(".js-list");
 
-// Nombramos las variables de
-const btnAdd = document.querySelector(".js-btn-add");
-const newForm = document.querySelector(".js-new-form")
-
-btnAdd.AddEventListener("click"), () => {
-  console.log("click")
-}
-
-kittenOne.innerHTML = (
-  <li class="card">
+// Asignar a cada constante su contenido que estaba antes en HTML
+const kittenOneText = `<li class="card">
     <article>
       <img
         class="card_img"
@@ -28,11 +20,9 @@ kittenOne.innerHTML = (
         donde tuvo su origen muy posiblemente.
       </p>
     </article>
-  </li>
-);
-
-kittenTwo.innerHTML = (
-  <li class="card">
+  </li>` ;
+  
+  const kittenTwoText = `  <li class="card">
     <img
       class="card_img"
       src="https://dev.adalab.es/sphynx-gato.webp"
@@ -45,10 +35,9 @@ kittenTwo.innerHTML = (
       pinta de alienígena han llegado a definir a esta raza gatuna que se
       caracteriza por la «ausencia» de pelo.
     </p>
-  </li>
-);
-kittenThree.innerHTML = (
-  <li class="card">
+  </li>` ;
+
+  const kittenThreeText = `<li class="card">
     <img
       class="card_img"
       src="https://dev.adalab.es/maine-coon-cat.webp"
@@ -61,5 +50,33 @@ kittenThree.innerHTML = (
       mirada se ha convertido en una de sus señas de identidad. Sus ojos son
       grandes y las orejas resultan largas y en punta.
     </p>
-  </li>
-);
+  </li>` ;
+
+  // Con innerHTML, asignamos los tres artículos de los gatos a un solo elemento, que es la etiqueta <ul>
+kittenOne.innerHTML = kittenOneText;
+
+kittenTwo.innerHTML = kittenTwoText;
+
+kittenThree.innerHTML = kittenThreeText;
+
+
+// Nombramos las variables del botón "+" y la sección "formulario", con sus clases añadidas correspondientes
+const btnAdd = document.querySelector(".js-btn-add");
+const newForm = document.querySelector(".js-new-form");
+
+// Añadimos un evento al botón para hacer "click" y otro evento (toggle) que muestra y oculta el formulario al hacer ese "click"
+btnAdd.addEventListener ("click", () => {
+  newForm.classList.toggle("collapsed");
+});
+
+//Nombramos la variable del "botón cancelar" con su clase correspondiente
+const buttonCancel = document.querySelector(".button-cancel");
+
+// Añadimos un evento al botón para hacer "click" y otro evento para añadir
+buttonCancel.addEventListener ("click", () => {
+newForm.classList.add("collapsed");
+})
+
+// El collapsed es una clase que aparece en CSS con un display None que oculta esa clase. 
+// Cuando la clase está oculta, el formulario aparece y cuando la clase aparece junto a las otras dos clases puestas en la sección "new form", el formulario se oculta.
+//En esa misma sección, se encuentra el botón "cancel". Cuando le demos click al botón, aparece la clase "collapsed" junto a new form, y el formulario se oculta.
