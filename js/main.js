2e74dc7139f7a0a44bf67b096592347e36b9d471
Sus.diaz <sus.diazse@gmail.com>
2025-10-22 16:56:44 +0200

    Add conditional check

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 "use strict";
 // Nombramos las variables de gatos y las asignamos a la clase de la etiqueta "list"
-const kittenOne = document.querySelector(".js-list");
-const kittenTwo = document.querySelector(".js-list");
-const kittenThree = document.querySelector(".js-list");
+const kittenlist = document.querySelector(".js-list");
+// const kittenOne = document.querySelector(".js-list");
+// const kittenTwo = document.querySelector(".js-list");
+// const kittenThree = document.querySelector(".js-list");
 
 // Asignar a cada constante su contenido que estaba antes en HTML
 const kittenOneText = `<li class="card">
@@ -14,15 +15,15 @@ const kittenOneText = `<li class="card">
       />
       <h3 class="card_title">Anastacio</h3>
       <h4 class="card_race">Siamés</h4>
-      <p class=" js_card_description">
+      <p class="card_description">
         Porte elegante, su patrón de color tan característico y sus ojos de un
         azul intenso, pero su historia se remonta a Asía al menos hace 500 años,
         donde tuvo su origen muy posiblemente.
       </p>
     </article>
-  </li>` ;
+  </li>`;
 
-const kittenTwoText = `  <li class="card">
+const kittenTwoText = `<li class="card">
     <img
       class="card_img"
       src="https://dev.adalab.es/sphynx-gato.webp"
@@ -30,12 +31,12 @@ const kittenTwoText = `  <li class="card">
     />
     <h3 class="card_title">Fiona</h3>
     <h4 class="card_race">Sphynx</h4>
-    <p class=" js_card_description">
+    <p class=" card_description">
       Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con
       pinta de alienígena han llegado a definir a esta raza gatuna que se
       caracteriza por la «ausencia» de pelo.
     </p>
-  </li>` ;
+  </li>`;
 
 const kittenThreeText = `<li class="card">
     <img
@@ -45,20 +46,12 @@ const kittenThreeText = `<li class="card">
     />
     <h3 class="card_title">Cielo</h3>
     <h4 class="card_race">Maine Coon</h4>
-    <p class=" js_card_description">
+    <p class=" card_description">
       Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella
       mirada se ha convertido en una de sus señas de identidad. Sus ojos son
       grandes y las orejas resultan largas y en punta.
     </p>
-  </li>` ;
-
-// Con innerHTML, asignamos los tres artículos de los gatos a un solo elemento, que es la etiqueta <ul>
-kittenOne.innerHTML = kittenOneText;
-
-kittenTwo.innerHTML = kittenTwoText;
-
-kittenThree.innerHTML = kittenThreeText;
-
+  </li>`;
 
 // Nombramos las variables del botón "+" y la sección "formulario", con sus clases añadidas correspondientes
 const btnAdd = document.querySelector(".js-btn-add");
@@ -75,33 +68,41 @@ const buttonCancel = document.querySelector(".button-cancel");
 // Añadimos un evento al botón para hacer "click" y otro evento para añadir
 buttonCancel.addEventListener("click", () => {
   newForm.classList.add("collapsed");
-})
+});
 
-// El collapsed es una clase que aparece en CSS con un display None que oculta esa clase. 
+// El collapsed es una clase que aparece en CSS con un display None que oculta esa clase.
 // Cuando la clase está oculta, el formulario aparece y cuando la clase aparece junto a las otras dos clases puestas en la sección "new form", el formulario se oculta.
 //En esa misma sección, se encuentra el botón "cancel". Cuando le demos click al botón, aparece la clase "collapsed" junto a new form, y el formulario se oculta.
 
-const searchButton = document.querySelector(".js_button-search");
-const descrSearchText = document.querySelector(".js_in_search_desc");
+const kittenDesc1 =
+  "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
+const kittenDesc2 =
+  "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+const kittenDesc3 =
+  "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad.Sus ojos son grandes y las orejas resultan largas y en punta.";
+
+const searchButton = document.querySelector(".js_button-search"); //Variable del botón del "buscar" en el apartado "filtrar/buscar"
+const input_search_desc = document.querySelector(".js_in_search_desc"); //Variable del input de la "descripción" del apartado "filtrar/buscar"
 
 searchButton.addEventListener("click", (ev) => {
-  ev.preventDefault();
-  const descrSearchText = input_search_desc.value; // Recoge el valor del input de la descripción en una variable
+  ev.preventDefault(); // Evita que el formulario recargue la página
+  const descrSearchText = input_search_desc.value; // Creamos una variable nueva, con otro nombre diferente que contiene el valor (value) de la variable del input (search_desc) de la descripción
+  let breedText = ""; // Creamos una variable para almacenar el texto de la raza
+  kittenlist.innerHTML = ""; // Para que no se repita la búsqueda si los campos están en blanco
+  if (kittenDesc1.includes(descrSearchText)) {
+    // Con innerHTML, asignamos los tres artículos de los gatos a un solo elemento, que es la etiqueta <ul>
+    kittenlist.innerHTML += kittenOneText;
+  }
+
+  if (kittenDesc2.includes(descrSearchText)) {
+    kittenlist.innerHTML += kittenTwoText;
+  }
+
+  if (kittenDesc3.includes(descrSearchText)) {
+    kittenlist.innerHTML += kittenThreeText;
+  }
+
+  // Los if se usan decirle a nuestra página web que si escribes en el input (descripción) un texto que está incluído en la descripción de algún artículo,
+  //  mostramos dicho artículo más cualquier otro que contenga ese mismo texto.
 });
-const kittenDesc1 = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
-const kittenDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
-const kittenDesc3 = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad.Sus ojos son grandes y las orejas resultan largas y en punta.";
 
-let card_description = document.querySelector(".js_card_description");
-
-if (kittenDesc1.includes(descrSearchText)) {
-  elegante
-};
-
-if (kittenDesc2.includes(descrSearchText)) {
-  //Completa el código
-};
-
-if (kittenDesc3.includes(descrSearchText)) {
-  //Completa el código
-};
