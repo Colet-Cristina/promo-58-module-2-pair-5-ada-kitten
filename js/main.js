9fa25f79d8a74f87703df29962bb0c492f2b916f
Cristina <tu-emailejemplo.com>
2025-10-24 10:49:03 +0200

    update

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@ const kittenlist = document.querySelector(".js-list");
 // const kittenOne = document.querySelector(".js-list");
 // const kittenTwo = document.querySelector(".js-list");
 // const kittenThree = document.querySelector(".js-list");
+const kittenImage1 = "https://dev.adalab.es/gato-siames.webp";
+const kittenName1 = "Anastacio";
+const kittenDesc1 =
+  " Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
+const kittenRace1 = "Siamés";
+
+const kittenImage2 = "https://dev.adalab.es/sphynx-gato.webp";
+const kittenName2 = "Fiona";
+const kittenDesc2 =
+  "Produce fascinación y curiosidad.Exótico, raro, bello, extraño hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+const kittenRace2 = "Sphynx";
+
+const kittenImage3 = "https://dev.adalab.es/maine-coon-cat.webp";
+const kittenName3 = "Cielo";
+const kittenDesc3 =
+  " Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+const kittenRace3 = "Maine Coon";
 
 // Asignar a cada constante su contenido que estaba antes en HTML
 const kittenOneText = `<li class="card">
